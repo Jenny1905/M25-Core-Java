b61f2e9875f6e16cc2cb1cc2eb424447ee924355
Jenny1905 <kumbhalkarshiwani@gmail.com>
2022-09-26 16:50:56 -0700

    First Commit

---
+++ Day-1 _ConnectingEclipseWithGitHub/src/org/tnsindia/basic/TestingEclipseForGitHub.java
@@ -0,0 +1,11 @@
+package org.tnsindia.basic;
+//Program on to print the statement ---Single line comment
+public class TestingEclipseForGitHub {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to TNS India Foundation");
+		
+
+	}
+
+}
